9c4867d51ac70bda93831db716013721798eebe4
arungithub92 <arunreturns04@gmail.com>
2024-04-08 15:24:33 +0530

    Revert "added a new function sum"
    This reverts commit 3b6d6936b13d9bc729aa9aee1ea091868d5a3454.

--- data_processing.py
+++ data_processing.py
@@ -1,11 +1,1 @@
 print("adding all the data processing code related to Fraud Detection")
-
-numbers = [1,2,3,4,5,1,4,5]
- 
-Sum = sum(numbers)
-print(Sum)
- 
-Sum = sum(numbers, 10)
-print(Sum)
-
-print("adding a new code which is not correct")
