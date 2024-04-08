d84528a64a7f493e5fc80c40488e5c591646258e
arungithub92 <arunreturns04@gmail.com>
2024-04-08 15:55:38 +0530

    made changes in python code

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,12 @@
 print("adding all the data processing code related to Fraud Detection")
+
+numbers = [1,2,3,4,5,1,4,5]
+ 
+Sum = sum(numbers)
+print(Sum)
+ 
+Sum = sum(numbers, 10)
+print(Sum)
+
+
+print("this is for resetting")
